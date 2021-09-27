234660ee0aa20891e574d5f1ab28508a254a8e26
= <=>
2021-09-27 18:29:01 +0200

    experimentation with moving lines

--- src/Tesla/Configurator.js
+++ src/Tesla/Configurator.js
@@ -1,6 +1,6 @@
-import React, {useRef} from 'react';
+import React, {useRef, useState} from 'react';
 import { Canvas, useFrame } from '@react-three/fiber';
-import { OrbitControls, Box } from '@react-three/drei';
+import { OrbitControls, Box, Line } from '@react-three/drei';
 import { EffectComposer, DepthOfField, Bloom, Noise, Vignette, ChromaticAberration } from "@react-three/postprocessing"
 import './Tesla.css';
 
@@ -8,18 +8,31 @@ import './Tesla.css';
 function Scene() {
   
   const box = useRef()
+  const line = useRef()
+
+  let [points, setPoints] = useState([[0,0,0]])
 
   useFrame(() => {
-    box.current.rotation.y += 0.005
+    // box.current.rotation.x += 0.01
+    // box.current.rotation.y += 0.01
+    // box.current.rotation.z += 0.01
+    const p = points[points.length-1]
+    let newPoints = [...points, [p[0]+(Math.random()-0.5), p[1]+(Math.random()-0.5), p[1]+(Math.random()-0.5)]]
+    if (newPoints.length > 100) {
+      newPoints = newPoints.splice(1)
+    }
+    setPoints(newPoints)
   })
 
   return (
     <>
       <ambientLight intensity={0.4}/>
       <spotLight position={[10, 15, 10]} angle={0.3}/>
-      <Box position={[0, 0, 0]} ref={box}>
+      {/* <Box position={[0, 0, 0]} ref={box}>
         <meshStandardMaterial attach="material" color="red" />
-      </Box>
+      </Box> */}
+      <Line points={points} color="#D5F0ED">
+      </Line>
     </>
   )
 }
@@ -32,12 +45,11 @@ export default function Configurator() {
       <Scene/>
       <EffectComposer>
         {/* <DepthOfField focusDistance={0} focalLength={0.02} bokehScale={2} height={480} /> */}
-        <Bloom luminanceThreshold={0.9} luminanceSmoothing={0.9} height={300} opacity={3} />
-        <Noise opacity={0.025} />
+        <Bloom luminanceThreshold={0.7} luminanceSmoothing={0.9} height={300} opacity={3} />
+        {/* <Noise opacity={0.025} /> */}
         {/* <ChromaticAberration offset={[0.001,0.001]}/> */}
-        <Vignette eskil={false} offset={0.1} darkness={0.5} />
+        {/* <Vignette eskil={false} offset={0.25} darkness={0.75} /> */}
       </EffectComposer>
-      <color attach="background" color={"#1C1C1C"}/>
     </Canvas>
   );
 }
