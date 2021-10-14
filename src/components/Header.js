6f22694cea7c35230a8791faa7f33e46b918f0a5
Joshua Knauber <joshua.knauber@gmail.com>
2021-10-14 23:17:46 +0200

    more work on header

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,30 +5,18 @@ import { useSpring, animated } from 'react-spring';
 
 import Console from './Console';
 import "./Header.css";
-import Arrows from '../assets/img/arrows.svg';
+
 import Arrow from '../assets/img/arrow.svg';
 
 
 
 export default function Header() {
 
-  const [showScrollWarning, setShowScrollWarning] = useState(false)
-
-  const triggerScrollWarning = () => {
-    setShowScrollWarning(true)
-  }
-
   const { arrowOpac } = useSpring({
     loop: { reverse: true },
-    from: { arrowOpac: 0.2 },
-    to: { arrowOpac: 1 },
-    config: { duration: 1500 }
-  });
-
-  const { scrollWarningOpac } = useSpring({
-    scrollWarningOpac: showScrollWarning ? 1 : 0,
-    config: { duration: 1000 },
-    onResolve: () => {setShowScrollWarning(false)}
+    from: { arrowOpac: .1 },
+    to: { arrowOpac: .5 },
+    config: { duration: 2000 }
   });
 
   return (
@@ -36,10 +24,10 @@ export default function Header() {
       {/* <Console/> */}
       <div className="textHeader">
         <ScaleText widthOnly={true} minFontSize={18} maxFontSize={150} className=""><p className="titleBig">Hey! I'm Joshua</p></ScaleText>
-        <p className="headerBodyText">I design unique & usable solutions for hard problems</p>
-        <p className="headerBodyText">Currently I'm studying <a href="https://code.berlin/en/" target="_blank">Interaction Design at CODE University in Berlin</a></p>
-        <animated.div style={{opacity:arrowOpac}} className="scrollIndicatorContainer" onClick={triggerScrollWarning}><img src={Arrow}/></animated.div>
-        <animated.p className="scrollWarning" style={{opacity:scrollWarningOpac}}>▼ scroll down ▼</animated.p>
+        <p className="headerBodyText">I love designing unique & usable solutions for hard problems</p>
+        <p className="headerBodyText">I'm studying <a href="https://code.berlin/en/" target="_blank">Interaction Design at CODE University Berlin</a></p>
+        <p className="headerScrollInfo">Scroll to see some of my projects</p>
+        <animated.div style={{opacity:arrowOpac}} className="scrollIndicatorContainer"><img src={Arrow}/></animated.div>
       </div>
     </div>
   );
